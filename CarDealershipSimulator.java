4594f484872f700e592c03d9b63b0023969f6a2b
lyjacky11 <lyjacky@live.com>
2019-03-06 22:56:45 -0500

    Vehicle and Car class

--- CarDealershipSimulator.java
+++ CarDealershipSimulator.java
@@ -23,3 +23,208 @@ public class CarDealershipSimulator
 	 
   }
 }
+
+class Vehicle {
+	private String mfr, color;
+	private int power, numWheels;
+	public final int ELECTRIC_MOTOR = 0;
+	public final int GAS_ENGINE = 0;
+
+	/**
+	 * @param mfr
+	 * @param color
+	 * @param power
+	 * @param numWheels
+	 */
+	public Vehicle (String mfr, String color, int power, int numWheels) {
+		this.mfr = mfr;
+		this.color = color;
+		this.power = power;
+		this.numWheels = numWheels;
+	}
+
+	/**
+	 * @return the mfr
+	 */
+	public String getMfr() {
+		return mfr;
+	}
+	/**
+	 * @param mfr the mfr to set
+	 */
+	public void setMfr(String mfr) {
+		this.mfr = mfr;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the power
+	 */
+	public int getPower() {
+		return power;
+	}
+	/**
+	 * @param power the power to set
+	 */
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	/**
+	 * @return the numWheels
+	 */
+	public int getNumWheels() {
+		return numWheels;
+	}
+	/**
+	 * @param numWheels the numWheels to set
+	 */
+	public void setNumWheels(int numWheels) {
+		this.numWheels = numWheels;
+	}
+
+	/**
+	 * @return the equality of two vehicles
+	 */
+	public boolean equals(Object other) {
+		Vehicle otherVehicle = (Vehicle) other;
+		return (this.mfr.equals(otherVehicle.mfr)) && (this.power == otherVehicle.power) && (this.numWheels == otherVehicle.numWheels);
+	}
+
+	/** 
+	 * @return manufacturer name and color
+	 */
+	public String display() {
+		return mfr + " " + color;
+	}
+}
+
+class Car extends Vehicle implements Comparable<Car> {
+	private String model;
+	private int maxRange;
+	private double safetyRating, price;
+	private boolean AWD;
+	private final String SEDAN = "";
+	private final String SUV = "";
+	private final String SPORTS = "";
+	private final String MINIVAN = "";
+
+	/**
+	 * @param mfr
+	 * @param color
+	 * @param power
+	 * @param numWheels
+	 * @param model
+	 * @param maxRange
+	 * @param safetyRating
+	 * @param price
+	 * @param aWD
+	 */
+	public Car (String mfr, String color, int power, int numWheels, String model, int maxRange, double safetyRating, double price, boolean aWD) {
+		super(mfr, color, power, numWheels);
+		this.model = model;
+		this.maxRange = maxRange;
+		this.safetyRating = safetyRating;
+		this.price = price;
+		this.setAWD(aWD);
+	}
+
+	/**
+	 * @return the model
+	 */
+	public String getModel() {
+		return model;
+	}
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	/**
+	 * @return the maxRange
+	 */
+	public int getMaxRange() {
+		return maxRange;
+	}
+	/**
+	 * @param maxRange the maxRange to set
+	 */
+	public void setMaxRange(int maxRange) {
+		this.maxRange = maxRange;
+	}
+	
+	/**
+	 * @return the safetyRating
+	 */
+	public double getSafetyRating() {
+		return safetyRating;
+	}
+	/**
+	 * @param safetyRating the safetyRating to set
+	 */
+	public void setSafetyRating(double safetyRating) {
+		this.safetyRating = safetyRating;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	/**
+	 * @return the aWD
+	 */
+	public boolean isAWD() {
+		return AWD;
+	}
+	/**
+	 * @param aWD the aWD to set
+	 */
+	public void setAWD(boolean aWD) {
+		this.AWD = aWD;
+	}
+
+	/**
+	 * @return equality of Car object
+	 */
+	public boolean equals(Object other) {
+		Car otherCar = (Car) other;
+		return super.equals(otherCar) && this.model.equals(otherCar.model) && (this.AWD == otherCar.AWD);
+	}
+
+	/**
+	 * @return comparsion value of two Car objects
+	 */
+	public int compareTo(Car other) {
+		return new Double(this.price).compareTo(other.price);
+	}
+
+	/**
+	 * @return the car specs
+	 */
+	public String display() {
+		return super.display() + model + " " + maxRange + " " + safetyRating + " " + price + " " + AWD;
+	}
+}
